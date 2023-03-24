10cc5791f00a9c817e93d3ab33e4712dd0dfd533
shravanidhanpal <shravanidhanpal20@gmail.com>
2023-03-24 22:46:31 +0530

    added first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import './ExpenseItem.css'
+function ExpenseItem(){
+    return <div className="expense-item">
+        <div className="expense-item__description">
+            Food Rs 10
+        </div>
+        <div className="expense-item__description">
+            Petrol Rs 100 
+        </div>
+        <div className="expense-item__description">
+            Movies Rs 200
+        </div>
+        </div>
+}
+export default ExpenseItem
