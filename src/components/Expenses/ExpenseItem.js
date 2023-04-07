9d25914f18beedc9d984fa1a8dd93457575ba039
shravanidhanpal <shravanidhanpal20@gmail.com>
2023-04-07 21:35:14 +0530

    added EventListener

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,12 +2,20 @@ import ExpenseDate from './ExpenseDate';
 import Card from'../UI/Card';
 import './ExpenseItem.css';
 const ExpenseItem=(props)=>{
+    const clickHandler=()=>{
+        console.log('clicked!!!');
+    }
+    const deleteHandler=()=>{
+        console.log('item deleted');
+    }
         return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
+                <button onClick={clickHandler}>change title</button>
+                <button onClick={deleteHandler}>Delete item</button>
             </div>
         </Card>
     );
