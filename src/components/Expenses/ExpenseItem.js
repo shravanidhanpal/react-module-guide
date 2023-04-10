32bf8d7a6002e6f2e4f16a0838d7023c76ac28b4
shravanidhanpal <shravanidhanpal20@gmail.com>
2023-04-10 15:42:26 +0530

    added a button which changes the amount.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,20 @@
+import React, { useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from'../UI/Card';
 import './ExpenseItem.css';
 const ExpenseItem=(props)=>{
+    const [amount,setAmount]=useState(props.amount);
     const clickHandler=()=>{
-        console.log('clicked!!!');
-    }
-    const deleteHandler=()=>{
-        console.log('item deleted');
+        setAmount('100$');
+        console.log(amount);
     }
         return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
-                <button onClick={clickHandler}>change title</button>
-                <button onClick={deleteHandler}>Delete item</button>
+                <div className='expense-item__price'>{amount}</div>
+                <button onClick={clickHandler}>change amount</button>
             </div>
         </Card>
     );
